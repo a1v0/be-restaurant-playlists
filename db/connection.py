55be3eb7d664d5409627a3a2d297a3e19afd2706
avc285 <avc285@gmail.com>
2022-12-20 10:00:23 +0000

    add to-dos to connection file

--- db/connection.py
+++ db/connection.py
@@ -2,10 +2,12 @@ import psycopg2
 from os import environ
 
 if "PYTEST_CURRENT_TEST" in environ:
+    # *** TO DO *** Check whether pytest actually adds this to the environment variables, or whether we need to add it manually
     environ["PG_DATABASE"] = "restaurant_playlists_test"
 else:
     environ["PG_DATABASE"] = "restaurant_playlists"
-    # needs provision for production environment
+    # *** TO DO *** needs provision for production environment
+
 
 connection = psycopg2.connect(
     database=environ["PG_DATABASE"]
