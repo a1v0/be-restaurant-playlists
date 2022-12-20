195da74c64711a66a71e9143b2287b3ca7318116
avc285 <avc285@gmail.com>
2022-12-20 09:34:55 +0000

    move data into new dir

---
+++ db/seed_data/dev_data.py
@@ -0,0 +1,40 @@
+playlists = [
+    {
+        "name": "Alvo's playlist",
+        "description": "lorem ipsum lorem ipsum",
+        "location": "leeds",
+        "cuisine": "chinese",
+        "owner_email": "ymca@restaurant-playlists.com",
+    },
+    {
+        "name": "Musab's playlist",
+        "description": "lorem ipsum lorem ipsum",
+        "location": "manchester",
+        "cuisine": "american",
+        "owner_email": "ymca2@restaurant-playlists.com",
+    },
+]
+
+users = [
+    {
+        "user_email": "ymca@restaurant-playlists.com",
+        "nickname": "ymca",
+        "avatar_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png",
+    },
+    {
+        "user_email": "ymca2@restaurant-playlists.com",
+        "nickname": "ymca2",
+        "avatar_url": "https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg",
+    },
+]
+
+restaurants = [
+    {"playlist_id": 1, "place_id": "ChIJ3-SMG6FeeUgRGKtBhlH0fhY"},
+    {"playlist_id": 1, "place_id": "ChIJwf5pV8bme0gRWrquuGBndA8"},
+    {"playlist_id": 1, "place_id": "ChIJbQIhL6FeeUgRd4YGS3CdRgk"},
+    {"playlist_id": 2, "place_id": "ChIJP8J3ZIVeeUgRlzmWlDEjXPc"},
+    {"playlist_id": 2, "place_id": "ChIJK9qnyBlceUgR0ahqyr73-qs"},
+    {"playlist_id": 2, "place_id": "ChIJmWR08-5deUgRIPZKe0zjFEg"},
+]
+
+votes = [{"playlist_id": 1, "vote_count": 4}, {"playlist_id": 2, "vote_count": 1}]
