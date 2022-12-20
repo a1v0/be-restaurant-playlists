dee0928c101c6c74a8dfd53be5915152d37a2492
avc285 <avc285@gmail.com>
2022-12-20 11:26:08 +0000

    replace single connection with pool

--- db/connection.py
+++ db/connection.py
@@ -1,17 +1,20 @@
-import psycopg2
+from psycopg2.pool import SimpleConnectionPool
 from os import environ
+from dotenv import dotenv_values
 
+env = ".env.development"
+
+# sets environment variable in case of testing
 if "PYTEST_CURRENT_TEST" in environ:
     # *** TO DO *** Check whether pytest actually adds this to the environment variables, or whether we need to add it manually
-    environ["PG_DATABASE"] = "restaurant_playlists_test"
-else:
-    environ["PG_DATABASE"] = "restaurant_playlists"
+    env = ".env.test"
     # *** TO DO *** needs provision for production environment
 
+config = {**dotenv_values(env)}  # returns the contents of specified .env file
+environ["PG_DATABASE"] = config["PG_DATABASE"]
 
-connection = psycopg2.connect(
-    database=environ["PG_DATABASE"]
-    # host = "", user = "", password = ""
-)
+pool = SimpleConnectionPool(minconn=1, maxconn=10, database=environ["PG_DATABASE"])
+connection = pool.getconn()
+pool.putconn(connection)
 
-connection.autocommit = True
+connection.autocommit = True  # no idea what this does but it's super important
