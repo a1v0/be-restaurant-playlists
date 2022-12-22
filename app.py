a2a2a615364d8cf72979ebe014e25ea8e65cb6b5
Cnmoosavinia <cyrusmoosavinia@gmail.com>
2022-12-22 13:46:07 +0000

    page layout change

--- app.py
+++ app.py
@@ -9,12 +9,14 @@ CORS(app)
 @app.route("/api/playlists", methods=["GET"])
 def all_playlists():
     if request.method == "GET":
-        cursor.execute("""SELECT playlists.*, users.nickname, CAST(CAST(AVG(votes.vote_count) AS DECIMAL(10, 1)) AS VARCHAR(4)) AS vote_count FROM playlists
-LEFT JOIN votes
-ON playlists.playlist_id = votes.playlist_id 
-LEFT JOIN users
-ON playlists.owner_email = users.user_email
-GROUP BY playlists.playlist_id, users.nickname;""")
+        cursor.execute("""
+        SELECT playlists.*, users.nickname, CAST(CAST(AVG(votes.vote_count) AS DECIMAL(10, 1)) AS VARCHAR(4)) AS vote_count FROM playlists
+        LEFT JOIN votes
+        ON playlists.playlist_id = votes.playlist_id 
+        LEFT JOIN users
+        ON playlists.owner_email = users.user_email
+        GROUP BY playlists.playlist_id, users.nickname;
+        """)
         playlists = cursor.fetchall()
         results = json.dumps({"playlists":playlists})
         loaded_results = json.loads(results)
