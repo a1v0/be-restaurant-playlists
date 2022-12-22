aabff719e0f7c7cc5817167ba5ed591345b02a93
yos244 <yos_24@hotmail.com>
2022-12-22 15:19:06 +0000

    added happy path test and for post user endpoint

--- __tests__/test_app.py
+++ __tests__/test_app.py
@@ -6,14 +6,18 @@ from app import app
 import json
 from ast import literal_eval
 
-# PYTHONPATH=$(pwd) py.test <optional keyword searches with -k -v>
+# PYTHONPATH=$(pwd) py.test -rP
+# <optional keyword searches with -k -v>
 
 # utility functions
 
+
 def create_dict(byte):
-    return literal_eval(byte.decode('utf-8'))
+    return literal_eval(byte.decode("utf-8"))
+
 
 # tests
+
 
 @pytest.fixture
 def test_app():
@@ -36,6 +40,7 @@ def client(test_app):
 def runner(test_app):
     return test_app.test_cli_runner()
 
+
 @pytest.mark.ticket_5
 def test_get_playlists_keys(client):
     response = client.get("/api/playlists")
@@ -44,15 +49,15 @@ def test_get_playlists_keys(client):
     print(array)
     assert response.status == "200 OK", "Test Failed"
     for playlist in array:
-        assert 'cuisine' in playlist, "test failed"
-        assert 'description' in playlist, "test failed"
-        assert 'location' in playlist, "test failed"
-        assert 'name' in playlist, "test failed"
-        assert 'owner_email' in playlist, "test failed"
-        assert 'playlist_id' in playlist, "test failed"
-        assert 'vote_count' in playlist, "test failed"
-        assert 'nickname' in playlist, "test failed"
-    
+        assert "cuisine" in playlist, "test failed"
+        assert "description" in playlist, "test failed"
+        assert "location" in playlist, "test failed"
+        assert "name" in playlist, "test failed"
+        assert "owner_email" in playlist, "test failed"
+        assert "playlist_id" in playlist, "test failed"
+        assert "vote_count" in playlist, "test failed"
+        assert "nickname" in playlist, "test failed"
+
 
 @pytest.mark.request_specific_playlist  # this is showing as a warning
 def test_request_specific_playlist_success(client):
@@ -89,3 +94,40 @@ def test_request_specific_playlist_invalid_playlist_id(client):
 
     assert response.status == "400 BAD REQUEST", "incorrect http response"
     assert playlist["msg"] == "invalid playlist id"
+
+
+@pytest.mark.post_new_user  # this is showing as a warning
+def test_post_new_user(client):
+    response = client.post(
+        "/api/users",
+        json={
+            "user_email": "someone@example.com",
+            "nickname": "Myself",
+            "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwlrNQyIFCa1XnXF1Ex8lSuOHhHaWxd3_zWR2m3j6Tig&s",
+        },
+    )
+    user_bytes = response.data 
+    user = json.loads(user_bytes.decode("utf-8"))
+    print(user)
+    assert response.status == "201 CREATED", "incorrect http response"
+    assert user["user"]["user_email"] == "someone@example.com"
+    assert user["user"]["nickname"] == "Myself"
+    assert user["user"]["avatar_url"] == "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwlrNQyIFCa1XnXF1Ex8lSuOHhHaWxd3_zWR2m3j6Tig&s"
+    
+def test_post_new_user_with_excess_data(client):
+    response = client.post(
+        "/api/users",
+        json={
+            "useless_property" : "useless_value",
+            "user_email": "someone@example.com",
+            "nickname": "Myself",
+            "avatar_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwlrNQyIFCa1XnXF1Ex8lSuOHhHaWxd3_zWR2m3j6Tig&s",
+        },
+    )
+    user_bytes = response.data 
+    user = json.loads(user_bytes.decode("utf-8"))
+    assert response.status == "201 CREATED", "incorrect http response"
+    assert user["user"]["user_email"] == "someone@example.com"
+    assert user["user"]["nickname"] == "Myself"
+    assert user["user"]["avatar_url"] == "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwlrNQyIFCa1XnXF1Ex8lSuOHhHaWxd3_zWR2m3j6Tig&s"
+    assert user["user"].get("useless_property") is None 
