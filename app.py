8b3887164e78c3898f7af6a9e90f183f9ac572da
Musab Hussain <moshkhussain@gmail.com>
2023-01-04 16:07:46 +0000

    POST restaurants 400 sad path completed

--- app.py
+++ app.py
@@ -233,28 +233,31 @@ def playlists_by_user(user_email):
 
 @app.route("/api/playlists/<playlist_id>/restaurants", methods=["POST"])
 def post_restaurants(playlist_id):
-    post_body = request.get_json()
-    place_ids = post_body["place_ids"]
-    query_string = """
-        INSERT INTO restaurants (playlist_id, place_id)
-        VALUES
-        """
-    for i in range(len(place_ids)):
-        if i == len(place_ids) - 1:
-            query_string += f""" ( {playlist_id}, %s) """
-        else:
-            query_string += f""" ( {playlist_id}, %s), """
+    try: 
+        post_body = request.get_json()
+        place_ids = post_body["place_ids"]
+        query_string = """
+            INSERT INTO restaurants (playlist_id, place_id)
+            VALUES
+            """
+        for i in range(len(place_ids)):
+            if i == len(place_ids) - 1:
+                query_string += f""" ( {playlist_id}, %s) """
+            else:
+                query_string += f""" ( {playlist_id}, %s), """
 
-    query_string += """ RETURNING *; """
+        query_string += """ RETURNING *; """
 
-    cursor.execute(
-        query_string, place_ids
-    )
+        cursor.execute(
+            query_string, place_ids
+        )
 
-    new_restaurants = cursor.fetchall()
-    results = json.dumps({ "restaurants": new_restaurants })
-    loaded_results = json.loads(results)
-    return loaded_results, 201
+        new_restaurants = cursor.fetchall()
+        results = json.dumps({ "restaurants": new_restaurants })
+        loaded_results = json.loads(results)
+        return loaded_results, 201
+    except psycopg2.errors.ForeignKeyViolation:
+        return {"msg": "playlist does not exist"}, 400
 
 # Utility functions
 def return_invalid_request_body():
